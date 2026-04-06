1ac28dc64c06f887a084ddc41f5d4520321aba15
Hymndrazill <srihafiras@yahoo.fr>
2026-04-06 17:05:06 +0100

    feat(app): add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = 4000;
+const appName = process.env.APP_NAME || "Nginxed nana app";
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  console.log(`Request served by ${appName}`);
+});
+
+app.listen(port, () => {
+  console.log(`${appName} is listening on port ${port}`);
+});
